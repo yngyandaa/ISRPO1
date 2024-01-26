7d93ab9fed5c66187630873781008d4ed5ea4c28
Denzel <denzelcrocker686@gmail.com>
2024-01-26 16:32:59 +0300

    Added obrabotchik sobytiy

--- ISRPOlr1/ISRPOlr1/MainWindow.xaml.cs
+++ ISRPOlr1/ISRPOlr1/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace ISRPOlr1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
